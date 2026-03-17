a42de7d7a058a9f319984d5df056c89959f8db23
Girard <zgirard@noderome107.cm.cluster>
2026-03-17 14:52:12 -0500

    removed run_pipeline.sh BSUB -queue

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -2,7 +2,6 @@
 # BSUB directives for the Nextflow driver job (submits and monitors child jobs).
 # This job stays alive for the full pipeline run; child jobs get resources from nextflow.config.
 #BSUB -J shareseq
-#BSUB -q general
 #BSUB -n 1
 #BSUB -M 16000
 #BSUB -R "rusage[mem=16000]"
